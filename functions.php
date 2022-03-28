9a975fece1e9879ac2cb72b92fd09feff09c7b1c
djimio <70515241+djimio@users.noreply.github.com>
2022-03-28 08:54:45 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'marianne_child_enqueue_styles' );
+
+function hamilton_child_enqueue_styles() {
+ 
+    $parent_style = 'marianne-style';
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'marianne-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
